de8143cf51c900d9ad539e7b7956c0a83657765e
NoaShimshi <noanoashimshi@gmail.com>
2024-03-27 11:35:11 +0200

    create tables

--- server/createTables.js
+++ server/createTables.js
@@ -11,15 +11,33 @@ const con = mysql.createConnection({
     if (err) throw err;
     console.log("Connected!");
     var patients_table = "CREATE TABLE patients (id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), identity_card VARCHAR(20), address_city VARCHAR(255), address_street VARCHAR(255), address_number VARCHAR(10), date_of_birth DATE, telephone VARCHAR(15), mobile_phone VARCHAR(15))";
-  
-    // var volumes_table_sql = "CREATE TABLE volumes (volume_id INT AUTO_INCREMENT PRIMARY KEY, tool_code INT, owner_code INT, deleted BINARY, availability BINARY)";
-    // var tools_borrowed_table_sql = "CREATE TABLE tools_borrowed (id INT AUTO_INCREMENT PRIMARY KEY, user_code INT, request_id INT, request_date DATE, confirmation_date DATE, return_date DATE, volume_code INT , deleted BINARY)";
-    // var tools_category_sql = "CREATE TABLE tools_category (id INT AUTO_INCREMENT PRIMARY KEY, tool_id INT, category_id INT)";
-    // var categories_table_sql = "CREATE TABLE categories (id INT AUTO_INCREMENT PRIMARY KEY, category_name VARCHAR(255))";
+    var vaccinations_table = "CREATE TABLE vaccinations (id INT AUTO_INCREMENT PRIMARY KEY, vaccine_manufacturer VARCHAR(255))";
+    var corona_patients_table = "CREATE TABLE corona_patients (id INT AUTO_INCREMENT PRIMARY KEY, patient_id INT, illness_date DATE, recovery_date DATE, FOREIGN KEY (patient_id) REFERENCES patients(id))"
+    var vaccine_doses_table = "CREATE TABLE vaccine_doses (id INT AUTO_INCREMENT PRIMARY KEY, dose_name VARCHAR(255))";
+    var dose_patient_vaccine_table = "CREATE TABLE dose_patient_vaccine (id INT AUTO_INCREMENT PRIMARY KEY, patient_id INT, vaccine_dose_id INT, FOREIGN KEY (patient_id) REFERENCES patients(id), FOREIGN KEY (vaccine_dose_id) REFERENCES vaccine_doses(id))"
 
     con.query(patients_table, function (err, result) {
       if (err) throw err;
-      console.log("Table patients altered");
+      console.log("patients_table altered");
     });
 
+    con.query(vaccinations_table, function (err, result) {
+      if (err) throw err;
+      console.log("vaccinations_table altered");
+    });
+
+    con.query(corona_patients_table, function (err, result) {
+      if (err) throw err;
+      console.log("corona_patients_table altered");
+    });
+
+    con.query(vaccine_doses_table, function (err, result) {
+      if (err) throw err;
+      console.log("vaccine_doses_table altered");
+    });
+
+    con.query(dose_patient_vaccine_table, function (err, result) {
+      if (err) throw err;
+      console.log("dose_patient_vaccine_table altered");
+    });
   });
